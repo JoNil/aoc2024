fdf5842c0cbaec250eb23c20fd8c13e2bd00b6b6
Jonathan Nilsson <jonathan@voysys.se>
2024-12-04 12:45:36 +0100

    4a

---
+++ src/days/day04.rs
@@ -0,0 +1,89 @@
+pub static INPUT: &str = include_str!("../input/4.txt");
+pub static TEST_INPUT: &str = include_str!("../input/4_test.txt");
+
+struct Map {
+    data: Vec<u8>,
+    stride: i32,
+}
+
+impl Map {
+    fn get(&self, x: i32, y: i32) -> u8 {
+        let index = x + y * self.stride;
+
+        if x < 0 || x >= self.stride {
+            return b'.';
+        }
+
+        if y < 0 {
+            return b'.';
+        }
+
+        if index >= self.data.len() as i32 || index < 0 {
+            return b'.';
+        }
+
+        self.data[index as usize]
+    }
+}
+
+pub fn a(input: &str) -> i32 {
+    let mut map = Map {
+        data: Vec::new(),
+        stride: 0,
+    };
+
+    let mut lines: i32 = 0;
+
+    for line in input.lines() {
+        map.stride = line.len() as i32;
+        lines += 1;
+
+        for char in line.bytes() {
+            map.data.push(char);
+        }
+    }
+
+    let mut count = 0;
+
+    for y in 0..lines {
+        for x in 0..map.stride {
+            let candiadtes_coords: [[(i32, i32); 4]; 4] = [
+                [(0, 0), (0, 1), (0, 2), (0, 3)],
+                [(0, 0), (1, 0), (2, 0), (3, 0)],
+                [(0, 0), (1, -1), (2, -2), (3, -3)],
+                [(0, 0), (1, 1), (2, 2), (3, 3)],
+            ];
+
+            for coords in candiadtes_coords {
+                let candidate = [
+                    map.get(x + coords[0].0, y + coords[0].1),
+                    map.get(x + coords[1].0, y + coords[1].1),
+                    map.get(x + coords[2].0, y + coords[2].1),
+                    map.get(x + coords[3].0, y + coords[3].1),
+                ];
+
+                if &candidate[..] == b"XMAS" || &candidate[..] == b"SAMX" {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    count
+}
+
+#[test]
+fn test_a() {
+    assert_eq!(a(TEST_INPUT), 18);
+    assert_eq!(a(INPUT), 2500);
+}
+
+pub fn b(input: &str) -> i32 {
+    0
+}
+
+#[test]
+fn test_b() {
+    assert_eq!(b(TEST_INPUT), 0);
+    assert_eq!(b(INPUT), 0);
+}
