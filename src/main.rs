ed0199eb219d8dcde85cb449fe0a6771bd11a157
Jonathan Nilsson <jonathan@voysys.se>
2024-12-05 11:49:32 +0100

    all

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 use std::time::Instant;
 
-use aoc2022::day05 as day;
+use aoc2022::*;
 
 fn main() {
     let start = Instant::now();
-    let val = day::b(day::INPUT);
+    day01::a(day01::INPUT);
+    day01::b(day01::INPUT);
+
+    day02::a(day02::INPUT);
+    day02::b(day02::INPUT);
+
+    day03::a(day03::INPUT);
+    day03::b(day03::INPUT);
+
+    day04::a(day04::INPUT);
+    day04::b(day04::INPUT);
+
+    day05::a(day05::INPUT);
+    day05::b(day05::INPUT);
+
     let elapsed = start.elapsed();
 
-    println!("{}", val);
     println!("{}", elapsed.as_micros());
 }
