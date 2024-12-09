7d307afcaa63b5382e7ed9a30548b9e61eddc5fa
Jonathan Nilsson <lord.jonil@gmail.com>
2024-12-10 00:56:53 +0100

    Time all

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,48 @@
-use std::time::Instant;
+use std::{any::type_name, fmt::Display, time::Instant};
 
 use aoc2024::*;
 
-fn main() {
+fn time<F, N>(f: F, input: &str)
+where
+    F: FnOnce(&str) -> N,
+    N: Display,
+{
     let start = Instant::now();
-    //day01::a(day01::INPUT);
-    //day01::b(day01::INPUT);
-
-    //day02::a(day02::INPUT);
-    //day02::b(day02::INPUT);
-
-    //day03::a(day03::INPUT);
-    //day03::b(day03::INPUT);
-
-    //day04::a(day04::INPUT);
-    //day04::b(day04::INPUT);
-
-    //day05::a(day05::INPUT);
-    //day05::b(day05::INPUT);
-
-    //day06::a(day06::INPUT);
-    //day06::b(day06::INPUT);
-
-    //day07::a(day07::INPUT);
-    day07::b(day07::INPUT);
-
-    //day08::a(day08::INPUT);
-    //day08::b(day08::INPUT);
-
-    //day09::a(day09::INPUT);
-    //day09::b(day09::INPUT);
-
+    let answer = f(input);
     let elapsed = start.elapsed();
 
-    println!("Time {} us", elapsed.as_micros());
+    println!(
+        "{} Time {} us: {answer}",
+        type_name::<F>().trim_start_matches("aoc2024::days::"),
+        elapsed.as_micros()
+    );
+}
+
+fn main() {
+    time(day01::a, day01::INPUT);
+    time(day01::b, day01::INPUT);
+
+    time(day02::a, day02::INPUT);
+    time(day02::b, day02::INPUT);
+
+    time(day03::a, day03::INPUT);
+    time(day03::b, day03::INPUT);
+
+    time(day04::a, day04::INPUT);
+    time(day04::b, day04::INPUT);
+
+    time(day05::a, day05::INPUT);
+    time(day05::b, day05::INPUT);
+
+    time(day06::a, day06::INPUT);
+    time(day06::b, day06::INPUT);
+
+    time(day07::a, day07::INPUT);
+    time(day07::b, day07::INPUT);
+
+    time(day08::a, day08::INPUT);
+    time(day08::b, day08::INPUT);
+
+    time(day09::a, day09::INPUT);
+    time(day09::b, day09::INPUT);
 }
