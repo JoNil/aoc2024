1087073e4d11fcbfc3284f57c5ffb3ae11f64b7a
Jonathan Nilsson <jonathan@voysys.se>
2024-12-10 13:27:35 +0100

    B

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -151,12 +151,59 @@ fn test_a() {
     assert_eq!(a(INPUT), 688);
 }
 
+fn find_paths_b(map: &Map, reachable_nines: &mut Vec<i32>, pos: IVec2) -> i32 {
+    {
+        let cached = reachable_nines
+            .get((pos.x + pos.y * map.width) as usize)
+            .unwrap();
+        if *cached > 0 {
+            return *cached;
+        }
+    }
+
+    let value = map.get(pos);
+    if value == 9 {
+        return 1;
+    }
+
+    let mut possible_paths = 0;
+
+    for possible_dir in [ivec2(1, 0), ivec2(-1, 0), ivec2(0, 1), ivec2(0, -1)] {
+        let candidate_pos = pos + possible_dir;
+        let candidate_val = map.get(candidate_pos);
+
+        if candidate_val as i32 - value as i32 == 1 {
+            possible_paths += find_paths_b(map, reachable_nines, candidate_pos);
+        }
+    }
+
+    reachable_nines[(pos.x + pos.y * map.width) as usize] = possible_paths;
+
+    possible_paths
+}
+
 pub fn b(input: &str) -> i32 {
-    0
+    let map = Map::new(input).ascii_digits_to_u8();
+    let mut reachable_nines = vec![0; (map.width * map.height) as usize];
+
+    let mut sum_of_reachable = 0;
+
+    for y in 0..map.height {
+        for x in 0..map.width {
+            let pos = ivec2(x, y);
+            let val = map.get(pos);
+
+            if val == 0 {
+                sum_of_reachable += find_paths_b(&map, &mut reachable_nines, pos);
+            }
+        }
+    }
+
+    sum_of_reachable
 }
 
 #[test]
 fn test_b() {
-    assert_eq!(b(TEST_INPUT), 0);
-    assert_eq!(b(INPUT), 0);
+    assert_eq!(b(TEST_INPUT), 81);
+    assert_eq!(b(INPUT), 1459);
 }
