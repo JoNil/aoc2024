693940c20a155407d1b97c371dfd6241263325e6
Jonathan Nilsson <jonathan@voysys.se>
2024-12-13 15:56:40 +0100

    Something is off

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -1,4 +1,3 @@
-use glam::ivec2;
 use parse_display::{Display, FromStr};
 
 pub static INPUT: &str = include_str!("../input/13.txt");
@@ -43,19 +42,24 @@ pub fn a(input: &str) -> i32 {
     let mut minimum_tokens = 0;
 
     for machine in machines {
-        let mut min = None;
+        let x = (machine.a.x, machine.b.x, machine.p.x);
+        let y = (machine.a.y, machine.b.y, machine.p.y);
 
-        for i in 0..100 {
-            for j in 0..100 {
-                let p = ivec2(machine.a.x, machine.a.y) * i + ivec2(machine.b.x, machine.b.y) * j;
-                if p == ivec2(machine.p.x, machine.p.y) {
-                    min = Some(min.unwrap_or(i32::MAX).min(i * 3 + j));
-                }
+        let x2 = (x.0 * y.0, x.1 * y.0, x.2 * y.0);
+        let y2 = (y.0 * x.0, y.1 * x.0, y.2 * x.0);
+
+        let x3 = (x2.0 - y2.0, x2.1 - y2.1, x2.2 - y2.2);
+
+        if x3.2 % x3.1 == 0 {
+            let b = x3.2 / x3.1;
+
+            let y3 = (y2.0, 0i32, y2.2 - y2.1 * b);
+
+            if y3.2 % y3.0 == 0 {
+                let a = y3.2 / y3.0;
+
+                minimum_tokens += a * 3 + b;
             }
-        }
-
-        if let Some(min) = min {
-            minimum_tokens += min;
         }
     }
 
