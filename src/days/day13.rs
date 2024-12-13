779f9a805603d6e7fcfd5a75e6c94fe836a649dd
Jonathan Nilsson <jonathan@voysys.se>
2024-12-13 12:57:26 +0100

    a

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -1,3 +1,4 @@
+use glam::{ivec2, IVec2};
 use parse_display::{Display, FromStr};
 
 pub static INPUT: &str = include_str!("../input/13.txt");
@@ -34,15 +35,31 @@ struct Machine {
 
 pub fn a(input: &str) -> i32 {
     let machines = input
+        .trim()
         .split("\n\n")
         .map(|s| s.parse::<Machine>().unwrap())
         .collect::<Vec<_>>();
 
+    let mut minimum_tokens = 0;
+
     for machine in machines {
-        println!("{}", machine);
+        let mut min = None;
+
+        for i in 0..100 {
+            for j in 0..100 {
+                let p = ivec2(machine.a.x, machine.a.y) * i + ivec2(machine.b.x, machine.b.y) * j;
+                if p == ivec2(machine.p.x, machine.p.y) {
+                    min = Some(min.unwrap_or(i32::MAX).min(i * 3 + j));
+                }
+            }
+        }
+
+        if let Some(min) = min {
+            minimum_tokens += min;
+        }
     }
 
-    0
+    minimum_tokens
 }
 
 #[test]
