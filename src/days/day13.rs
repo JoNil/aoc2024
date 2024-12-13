249c5adc712e3633aebd096db3fb805b6ade3429
Jonathan Nilsson <jonathan@voysys.se>
2024-12-13 15:52:26 +0100

    We haz math

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -1,4 +1,4 @@
-use glam::{ivec2, IVec2};
+use glam::ivec2;
 use parse_display::{Display, FromStr};
 
 pub static INPUT: &str = include_str!("../input/13.txt");
@@ -65,15 +65,53 @@ pub fn a(input: &str) -> i32 {
 #[test]
 fn test_a() {
     assert_eq!(a(TEST_INPUT), 480);
-    assert_eq!(a(INPUT), 0);
+    assert_eq!(a(INPUT), 30413);
 }
 
-pub fn b(input: &str) -> i32 {
-    0
+pub fn b(input: &str) -> i64 {
+    let machines = input
+        .trim()
+        .split("\n\n")
+        .map(|s| s.parse::<Machine>().unwrap())
+        .collect::<Vec<_>>();
+
+    let mut minimum_tokens = 0;
+
+    for machine in machines {
+        let x = (
+            machine.a.x as i64,
+            machine.b.x as i64,
+            machine.p.x as i64 + 10000000000000,
+        );
+        let y = (
+            machine.a.y as i64,
+            machine.b.y as i64,
+            machine.p.y as i64 + 10000000000000,
+        );
+
+        let x2 = (x.0 * y.0, x.1 * y.0, x.2 * y.0);
+        let y2 = (y.0 * x.0, y.1 * x.0, y.2 * x.0);
+
+        let x3 = (x2.0 - y2.0, x2.1 - y2.1, x2.2 - y2.2);
+
+        if x3.2 % x3.1 == 0 {
+            let b = x3.2 / x3.1;
+
+            let y3 = (y2.0, 0i64, y2.2 - y2.1 * b);
+
+            if y3.2 % y3.0 == 0 {
+                let a = y3.2 / y3.0;
+
+                minimum_tokens += a * 3 + b;
+            }
+        }
+    }
+
+    minimum_tokens
 }
 
 #[test]
 fn test_b() {
-    assert_eq!(b(TEST_INPUT), 0);
-    assert_eq!(b(INPUT), 0);
+    assert_eq!(b(TEST_INPUT), 875318608908);
+    assert_eq!(b(INPUT), 92827349540204);
 }
