bcb8eff08fc1c80369ddd3468c6f036e65750aaa
Jonathan Nilsson <lord.jonil@gmail.com>
2024-12-18 21:53:29 +0100

    Call the good one with fallback

--- src/main.rs
+++ src/main.rs
@@ -62,7 +62,6 @@ fn main() {
     time(day13::b, day13::INPUT);
 
     time(|input| day14::a(input, ivec2(101, 103)), day14::INPUT);
-    time(|input| day14::b(input, ivec2(101, 103)), day14::INPUT);
     time(|input| day14_simd::b(input, ivec2(101, 103)), day14::INPUT);
 
     time(day15::a, day15::INPUT);
