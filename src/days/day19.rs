75ae9d41b8d9e90a1bfb07835f65b01f33140521
Jonathan Nilsson <jonathan@voysys.se>
2024-12-19 11:32:56 +0100

    Day 19 a

--- src/days/day19.rs
+++ src/days/day19.rs
@@ -1,14 +1,47 @@
 pub static INPUT: &str = include_str!("../input/19.txt");
 pub static TEST_INPUT: &str = include_str!("../input/19_test.txt");
 
+fn match_pattern(patterns: &[&str], design: &str) -> bool {
+    if design.is_empty() {
+        return true;
+    }
+
+    for pattern in patterns {
+        let pattern_len = pattern.len();
+        let design_len = design.len();
+
+        if pattern_len > design_len {
+            continue;
+        }
+
+        if &design[..pattern_len] == *pattern && match_pattern(patterns, &design[pattern_len..]) {
+            return true;
+        }
+    }
+
+    false
+}
+
 pub fn a(input: &str) -> i32 {
-    0
+    let (pattern_str, design_str) = input.trim().split_once("\n\n").unwrap();
+
+    let patterns = pattern_str.split(", ").collect::<Vec<_>>();
+
+    let mut possible_designs = 0;
+
+    for design in design_str.lines() {
+        if match_pattern(&patterns, design) {
+            possible_designs += 1;
+        }
+    }
+
+    possible_designs
 }
 
 #[test]
 fn test_a() {
-    assert_eq!(a(TEST_INPUT), 0);
-    assert_eq!(a(INPUT), 0);
+    assert_eq!(a(TEST_INPUT), 6);
+    assert_eq!(a(INPUT), 363);
 }
 
 pub fn b(input: &str) -> i32 {
@@ -17,6 +50,6 @@ pub fn b(input: &str) -> i32 {
 
 #[test]
 fn test_b() {
-    assert_eq!(b(TEST_INPUT), 0);
+    assert_eq!(b(TEST_INPUT), 6);
     assert_eq!(b(INPUT), 0);
 }
