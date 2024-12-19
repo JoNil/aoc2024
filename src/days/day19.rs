8dfda3477fa20d9d4b5bd2aa8d496ac5679b7c5d
Jonathan Nilsson <jonathan@voysys.se>
2024-12-19 14:09:16 +0100

    maybe on the right track

--- src/days/day19.rs
+++ src/days/day19.rs
@@ -1,5 +1,3 @@
-use std::cmp::Ordering;
-
 pub static INPUT: &str = include_str!("../input/19.txt");
 pub static TEST_INPUT: &str = include_str!("../input/19_test.txt");
 
@@ -47,12 +45,10 @@ fn test_a() {
     assert_eq!(a(INPUT), 363);
 }
 
-fn match_pattern_pre_b(patterns: &[&str], design: &str) -> i32 {
+fn match_pattern_except(patterns: &[&str], design: &str, except: &str) -> bool {
     if design.is_empty() {
-        return 1;
+        return true;
     }
-
-    let mut count = 0;
 
     for pattern in patterns {
         let pattern_len = pattern.len();
@@ -62,70 +58,53 @@ fn match_pattern_pre_b(patterns: &[&str], design: &str) -> i32 {
             continue;
         }
 
-        if &design[..pattern_len] == *pattern {
-            count += match_pattern_pre_b(patterns, &design[pattern_len..]);
-        }
-    }
-
-    count
-}
-
-fn match_pattern_b(patterns: &[(String, i64)], design: &str) -> i64 {
-    if design.is_empty() {
-        return 1;
-    }
-
-    //println!("D: {design}");
-
-    for (pattern, possible) in patterns {
-        let pattern_len = pattern.len();
-        let design_len = design.len();
-
-        if pattern_len > design_len {
+        if *pattern == except {
             continue;
         }
 
-        if design[..pattern_len] == *pattern {
-            //println!("P: {pattern} {possible}");
-            return possible * match_pattern_b(patterns, &design[pattern_len..]);
+        if &design[..pattern_len] == *pattern
+            && match_pattern_except(patterns, &design[pattern_len..], except)
+        {
+            return true;
         }
     }
 
-    0
+    false
 }
 
 pub fn b(input: &str) -> i64 {
     let (pattern_str, design_str) = input.trim().split_once("\n\n").unwrap();
 
     let patterns = pattern_str.split(", ").collect::<Vec<_>>();
-    let mut patterns_with_ways = Vec::with_capacity(patterns.len());
+
+    let mut primitive = Vec::with_capacity(patterns.len());
+    let mut combined = Vec::with_capacity(patterns.len());
 
     for pattern in &patterns {
-        let possible_ways = match_pattern_pre_b(&patterns, pattern) as i64;
-        patterns_with_ways.push((pattern.to_string(), possible_ways));
-    }
-
-    for (i, pattern) in patterns.iter().enumerate() {
-        for other in &patterns[(i + 1)..] {
-            let combined = format!("{pattern}{other}");
-            if !patterns.contains(&combined.as_str()) {
-                let possible_ways = match_pattern_pre_b(&patterns, &combined) as i64;
-                patterns_with_ways.push((combined, possible_ways));
-            }
+        if match_pattern_except(&patterns, pattern, pattern) {
+            combined.push(*pattern);
+        } else {
+            primitive.push(*pattern);
         }
     }
 
-    patterns_with_ways.sort_by(|a, b| match b.1.cmp(&a.1) {
-        Ordering::Less => Ordering::Less,
-        Ordering::Equal => b.0.len().cmp(&a.0.len()),
-        Ordering::Greater => Ordering::Greater,
-    });
+    println!("{primitive:?}");
+    println!("{combined:?}");
 
     let mut possible_designs = 0;
 
     for design in design_str.lines() {
         println!("New: {design}");
-        let new_possible_designs = match_pattern_b(&patterns_with_ways, design);
+        let mut new_possible_designs = if match_pattern(&primitive, design) {
+            1
+        } else {
+            0
+        };
+        for combined in &combined {
+            if design.contains(*combined) {
+                new_possible_designs *= 2;
+            }
+        }
         possible_designs += new_possible_designs;
         println!("Ways: {new_possible_designs}");
     }
@@ -136,5 +115,5 @@ pub fn b(input: &str) -> i64 {
 #[test]
 fn test_b() {
     assert_eq!(b(TEST_INPUT), 16);
-    assert_eq!(b(INPUT), 0);
+    //assert_eq!(b(INPUT), 0);
 }
