faa42b171d7dd00d1c7a9627e931f7ea1477dd5f
Jonathan Nilsson <jonathan@voysys.se>
2024-12-19 13:43:00 +0100

    Wrong again

--- src/days/day19.rs
+++ src/days/day19.rs
@@ -1,4 +1,4 @@
-use fxhash::FxHashMap;
+use std::cmp::Ordering;
 
 pub static INPUT: &str = include_str!("../input/19.txt");
 pub static TEST_INPUT: &str = include_str!("../input/19_test.txt");
@@ -47,46 +47,87 @@ fn test_a() {
     assert_eq!(a(INPUT), 363);
 }
 
-fn match_pattern_b(patterns: &FxHashMap<u8, Vec<&str>>, design: &str) -> i32 {
+fn match_pattern_pre_b(patterns: &[&str], design: &str) -> i32 {
     if design.is_empty() {
         return 1;
     }
 
     let mut count = 0;
 
-    if let Some(possible_patterns) = patterns.get(&design.as_bytes()[0]) {
-        for pattern in possible_patterns {
-            let pattern_len = pattern.len();
-            let design_len = design.len();
+    for pattern in patterns {
+        let pattern_len = pattern.len();
+        let design_len = design.len();
 
-            if pattern_len > design_len {
-                continue;
-            }
+        if pattern_len > design_len {
+            continue;
+        }
 
-            if &design[..pattern_len] == *pattern {
-                count += match_pattern_b(patterns, &design[pattern_len..]);
-            }
+        if &design[..pattern_len] == *pattern {
+            count += match_pattern_pre_b(patterns, &design[pattern_len..]);
         }
     }
 
     count
 }
 
-pub fn b(input: &str) -> i32 {
+fn match_pattern_b(patterns: &[(String, i64)], design: &str) -> i64 {
+    if design.is_empty() {
+        return 1;
+    }
+
+    //println!("D: {design}");
+
+    for (pattern, possible) in patterns {
+        let pattern_len = pattern.len();
+        let design_len = design.len();
+
+        if pattern_len > design_len {
+            continue;
+        }
+
+        if design[..pattern_len] == *pattern {
+            //println!("P: {pattern} {possible}");
+            return possible * match_pattern_b(patterns, &design[pattern_len..]);
+        }
+    }
+
+    0
+}
+
+pub fn b(input: &str) -> i64 {
     let (pattern_str, design_str) = input.trim().split_once("\n\n").unwrap();
 
     let patterns = pattern_str.split(", ").collect::<Vec<_>>();
-    let mut sorted_patterns: FxHashMap<u8, Vec<&str>> = FxHashMap::default();
+    let mut patterns_with_ways = Vec::with_capacity(patterns.len());
 
-    for pattern in patterns {
-        let key = pattern.as_bytes()[0];
-        sorted_patterns.entry(key).or_default().push(pattern);
+    for pattern in &patterns {
+        let possible_ways = match_pattern_pre_b(&patterns, pattern) as i64;
+        patterns_with_ways.push((pattern.to_string(), possible_ways));
     }
+
+    for (i, pattern) in patterns.iter().enumerate() {
+        for other in &patterns[(i + 1)..] {
+            let combined = format!("{pattern}{other}");
+            if !patterns.contains(&combined.as_str()) {
+                let possible_ways = match_pattern_pre_b(&patterns, &combined) as i64;
+                patterns_with_ways.push((combined, possible_ways));
+            }
+        }
+    }
+
+    patterns_with_ways.sort_by(|a, b| match b.1.cmp(&a.1) {
+        Ordering::Less => Ordering::Less,
+        Ordering::Equal => b.0.len().cmp(&a.0.len()),
+        Ordering::Greater => Ordering::Greater,
+    });
 
     let mut possible_designs = 0;
 
     for design in design_str.lines() {
-        possible_designs += match_pattern_b(&sorted_patterns, design);
+        println!("New: {design}");
+        let new_possible_designs = match_pattern_b(&patterns_with_ways, design);
+        possible_designs += new_possible_designs;
+        println!("Ways: {new_possible_designs}");
     }
 
     possible_designs
@@ -95,5 +136,5 @@ pub fn b(input: &str) -> i32 {
 #[test]
 fn test_b() {
     assert_eq!(b(TEST_INPUT), 16);
-    //assert_eq!(b(INPUT), 0);
+    assert_eq!(b(INPUT), 0);
 }
