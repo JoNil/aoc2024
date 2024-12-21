adc43b767cf9189ea19305deb5db24fa7122242b
Jonathan Nilsson <lord.jonil@gmail.com>
2024-12-21 14:50:05 +0100

    Progress

--- src/days/day21.rs
+++ src/days/day21.rs
@@ -1,7 +1,31 @@
+use glam::ivec2;
+
 pub static INPUT: &str = include_str!("../input/21.txt");
 pub static TEST_INPUT: &str = include_str!("../input/21_test.txt");
 
-fn find_shortest_sequence(code: &[u8]) -> i32 {}
+fn pos_from_digit(digit: u8) -> IVec2 {
+    match digit {
+        b'7' => ivec2(0, 0),
+        b'8' => ivec2(1, 0),
+        b'9' => ivec2(2, 0),
+
+        b'4' => ivec2(0, 1),
+        b'5' => ivec2(1, 1),
+        b'6' => ivec2(2, 1),
+
+        b'1' => ivec2(0, 2),
+        b'2' => ivec2(1, 2),
+        b'3' => ivec2(2, 2),
+
+        b'0' => ivec2(1, 3),
+        b'A' => ivec2(2, 3),
+        _ => panic!("Invalid"),
+    }
+}
+
+fn find_shortest_sequence(code: &[u8]) -> i32 {
+    0
+}
 
 pub fn a(input: &str) -> i32 {
     let codes = input
